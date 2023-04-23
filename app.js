3f91b49a01f311d212bc4d8d94203dc5b87a28d6
MuhammadMarmash <hamada500014@gmail.com>
2023-04-23 14:16:40 +0300

    added simicolens

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 function getComputerChoice() {
-    let choices = ["Rock", "Paper", "Scissors"]
-    return choices[Math.floor(Math.random() * choices.length)]
+    let choices = ["Rock", "Paper", "Scissors"];
+    return choices[Math.floor(Math.random() * choices.length)];
 }
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase()
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
     if (playerSelection === computerSelection) {
-        return "Draw! Nobody Wins"
+        return "Draw! Nobody Wins";
     } else if ((playerSelection === "Rock" && computerSelection === "Scissors") || (playerSelection === "Paper" && computerSelection === "Rock") || (playerSelection === "Scissors" && computerSelection === "Paper")) {
-        return `You Win! ${playerSelection} beats ${computerSelection}`
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
     } else {
-        return `You Lose! ${computerSelection} beats ${playerSelection}`
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
     }
 }
 
 function game() {
     for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Rock, Paper, Scissors??")
-        console.log(playRound(playerSelection, getComputerChoice()))
+        let playerSelection = prompt("Rock, Paper, Scissors??");
+        console.log(playRound(playerSelection, getComputerChoice()));
     }
 }
 
-game()
+game();
