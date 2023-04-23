60a62f3cd951c30b504b3dc7473c6859cd0502f4
MuhammadMarmash <hamada500014@gmail.com>
2023-04-23 13:42:15 +0300

    finished the game functionality in the console

---
+++ app.js
@@ -0,0 +1,24 @@
+function getComputerChoice() {
+    let choices = ["Rock", "Paper", "Scissors"]
+    return choices[Math.floor(Math.random() * choices.length)]
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase()
+    if (playerSelection === computerSelection) {
+        return "Draw! Nobody Wins"
+    } else if ((playerSelection === "Rock" && computerSelection === "Scissors") || (playerSelection === "Paper" && computerSelection === "Rock") || (playerSelection === "Scissors" && computerSelection === "Paper")) {
+        return `You Win! ${playerSelection} beats ${computerSelection}`
+    } else {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`
+    }
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Rock, Paper, Scissors??")
+        console.log(playRound(playerSelection, getComputerChoice()))
+    }
+}
+
+game()
