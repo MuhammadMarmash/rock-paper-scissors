bc771a4346c42c2080cd84972389b7301c2b51a9
MuhammadMarmash <hamada500014@gmail.com>
2023-04-25 13:50:44 +0300

    added ui

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+const btn_choices = document.querySelectorAll(".choice");
+const btn_result = document.querySelector("#result");
+const human_score = document.querySelector("#human");
+const computer_score = document.querySelector("#computer");
+
 function getComputerChoice() {
     let choices = ["Rock", "Paper", "Scissors"];
     return choices[Math.floor(Math.random() * choices.length)];
@@ -6,19 +11,36 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
     if (playerSelection === computerSelection) {
-        return "Draw! Nobody Wins";
+        btn_result.textContent = "Draw! Nobody Wins";
     } else if ((playerSelection === "Rock" && computerSelection === "Scissors") || (playerSelection === "Paper" && computerSelection === "Rock") || (playerSelection === "Scissors" && computerSelection === "Paper")) {
-        return `You Win! ${playerSelection} beats ${computerSelection}`;
+        human_score.textContent = parseInt(human_score.textContent) + 1;
+        if (human_score.textContent === "5"){
+            end_game();
+            btn_result.textContent = "Congratulations, you win";
+        }
+        btn_result.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
     } else {
-        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+        computer_score.textContent = parseInt(computer_score.textContent) + 1;
+        if (computer_score.textContent === "5"){
+            end_game();
+            btn_result.textContent= "Game Over, you Lost";
+        }
+        btn_result.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
     }
 }
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Rock, Paper, Scissors??");
-        console.log(playRound(playerSelection, getComputerChoice()));
-    }
+function end_game(){
+    btn_choices.forEach(btn => {
+        btn.disabled = true;
+    })
 }
+
+function game() {
+    btn_choices.forEach(btn => {
+        btn.onclick = () => playRound(btn.textContent, getComputerChoice());
+    });
+}
+
+
 
 game();
